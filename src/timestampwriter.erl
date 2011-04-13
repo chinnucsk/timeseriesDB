922dcecfca50f5aef9f650e02eaa6a729cee9e29
Lars Hesel Christensen <larshesel@gmail.com>
2011-04-13 21:22:32 +0200

    switch on errorcode in timestampwriter

--- src/timestampwriter.erl
+++ src/timestampwriter.erl
@@ -5,10 +5,17 @@ convert_to_unix_time({Mega,Sec,Micro}) ->
     (Mega*1000000+Sec)*1000000+Micro.
 
 write(Unixtimestamp, Value) when is_float(Value) ->
-    {ok, WriteDescr} = file:open(integer_to_list(Unixtimestamp), [raw, append]),
-    file:write(WriteDescr,float_to_list(Value)),
-    file:close(WriteDescr).
+    case file:open(integer_to_list(Unixtimestamp), [raw, append]) of 
+	{ok, WriteDescr} ->
+	    file:write(WriteDescr,float_to_list(Value)),
+	    file:close(WriteDescr);
+	{error, Reason} ->
+	    log("Error", [Reason])
+	end.
 
 write(Value) ->
     write(convert_to_unix_time(erlang:now()), Value).
 
+
+log(Val, Reason) ->
+    Reason.
