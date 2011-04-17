c3dbc7a7f302fb59eec94e017777be23ec0c2664
Peter Mechlenborg <peter.mechlenborg@gmail.com>
2011-04-17 15:59:57 +0200

    Test setup for tsdb_storage_lib.

---
+++ test/tsdb_storage_lib_tests.erl
@@ -0,0 +1,48 @@
+-module(tsdb_storage_lib_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+open_timeseries_file_test_() ->
+    with_tmp_file(
+      "aha1.ts",
+      fun(File) ->
+	      ?cmd("touch " ++ File),
+	      ?assertMatch(not_implemented, tsdb_storage_lib:open_timeseries_file(File))
+      end).
+
+add_value_test_() ->
+    with_tmp_file(
+      "aha2.ts",
+      fun(File) ->
+	      ?cmd("touch " ++ File),
+	      ?assertMatch(not_implemented, tsdb_storage_lib:add_value("foo", etu))
+      end).
+
+get_values_test_() ->
+    with_tmp_file(
+      "aha3.ts",
+      fun(File) ->
+	      ?cmd("touch " ++ File),
+	      ?assertMatch(not_implemented, tsdb_storage_lib:get_values("foo", oetn, ste))
+      end).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Helper functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tmp_dir() ->
+    code:lib_dir(timeseriesDB) ++ "/test/tmp/".
+
+tmp_file(Name) ->
+    tmp_dir() ++ Name.
+
+with_tmp_file(Name, Fun) ->
+    Tmp = tmp_file(Name),
+    {setup,
+     fun() -> Tmp end,
+     fun(Tmpfile) ->
+	     os:cmd("rm " ++ Tmpfile)
+     end,
+     fun() -> Fun(Tmp) end}.
+
